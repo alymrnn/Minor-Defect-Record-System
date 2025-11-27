3ae937e17ef9964d8959b69c765da5baf457551e
alymrnn <allym.furukawa@gmail.com>
2025-11-27 14:17:14 +0800

    updated edit record layout/occ shift

--- modals/edit_defect_record.php
+++ modals/edit_defect_record.php
@@ -153,40 +153,8 @@
                         <input id="edit_treatment_content" class="form-control form-control-sm form-control-border"
                             required>
                     </div>
-                    <div class="col-12 col-md-4">
-                        <label class="m-0 p-0 text-sm font-weight-normal">Total Time (mins)</label>
-                        <label class="m-0 p-0 text-info text-xs">*</label>
-                        <input type="number" id="edit_total_time" class="form-control form-control-sm form-control-border"
-                            required>
-                    </div>
                 </div>
                 <div class="row mb-3 mt-3">
-                    <div class="col-12 col-md-3 offset-3">
-                        <label class="m-0 p-0 text-sm font-weight-normal">Occurrence Shift</label>
-                        <label class="m-0 p-0 text-info text-xs">*</label>
-                        <select id="edit_occurrence_shift" class="form-control form-control-sm form-control-border" required>
-                            <option value="" disabled>Select Shift</option>
-                            <option value="N/A" selected>N/A</option>
-                            <option value="DS">DS</option>
-                            <option value="NS">NS</option>
-                        </select>
-                    </div>
-                    <div class="col-12 col-md-3">
-                        <label class="m-0 p-0 text-sm font-weight-normal">Occurrence Board No.</label>
-                        <label class="m-0 p-0 text-info text-xs">*</label>
-                        <input type="text" id="edit_occurrence_board_no" class="form-control form-control-sm form-control-border" autocomplete="off"
-                            oninput="this.value = this.value.toUpperCase();"
-                            required>
-                    </div>
-                    <div class="col-12 col-md-3">
-                        <label class="m-0 p-0 text-sm font-weight-normal">Occurrence Station No.</label>
-                        <label class="m-0 p-0 text-info text-xs">*</label>
-                        <input type="text" id="edit_occurrence_station_no" class="form-control form-control-sm form-control-border" autocomplete="off"
-                            oninput="this.value = this.value.toUpperCase();"
-                            required>
-                    </div>
-                </div>
-                <div class="row">
                     <div class="col-12 col-md-3">
                         <label class="m-0 p-0 text-sm font-weight-normal">Sequence No.</label>
                         <label class="m-0 p-0 text-info text-xs">*</label>
@@ -199,6 +167,38 @@
                         <label class="m-0 p-0 text-info text-xs">*</label>
                         <input type="text" id="edit_connector_no" class="form-control form-control-sm form-control-border" autocomplete="off"
                             oninput="this.value = this.value.toUpperCase();"
+                            required>
+                    </div>
+                    <div class="col-12 col-md-3">
+                        <label class="m-0 p-0 text-sm font-weight-normal">Occurrence Shift</label>
+                        <label class="m-0 p-0 text-info text-xs">*</label>
+                        <select id="edit_occurrence_shift" class="form-control form-control-sm form-control-border" required>
+                            <option value="" disabled selected>Select Group</option>
+                            <option value="ADS">ADS</option>
+                            <option value="A">A</option>
+                            <option value="B">B</option>
+                        </select>
+                    </div>
+                    <div class="col-12 col-md-3">
+                        <label class="m-0 p-0 text-sm font-weight-normal">Occurrence Board No.</label>
+                        <label class="m-0 p-0 text-info text-xs">*</label>
+                        <input type="text" id="edit_occurrence_board_no" class="form-control form-control-sm form-control-border" autocomplete="off"
+                            oninput="this.value = this.value.toUpperCase();"
+                            required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-3">
+                        <label class="m-0 p-0 text-sm font-weight-normal">Occurrence Station No.</label>
+                        <label class="m-0 p-0 text-info text-xs">*</label>
+                        <input type="text" id="edit_occurrence_station_no" class="form-control form-control-sm form-control-border" autocomplete="off"
+                            oninput="this.value = this.value.toUpperCase();"
+                            required>
+                    </div>
+                    <div class="col-12 col-md-3">
+                        <label class="m-0 p-0 text-sm font-weight-normal">Total Time (mins)</label>
+                        <label class="m-0 p-0 text-info text-xs">*</label>
+                        <input type="number" id="edit_total_time" class="form-control form-control-sm form-control-border"
                             required>
                     </div>
                     <div class="col-12 col-md-3">
